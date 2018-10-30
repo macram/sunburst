6fa2fff3169989574596fc93d79adb442cbd913c
Manu Mateos <manu.mateos.cruz@gmail.com>
2018-10-30 20:59:28 +0100

    Add parameter handling, first try for circle detection (not working at this moment)

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2
+import argparse
 
 
-def readimage(path="/Users/manu/photo_2018-06-24_21-03-25.jpg"):
+def readimage(path):
     img = cv2.imread(path, cv2.IMREAD_COLOR)
-    showimage(greyscaleimage(img))
+    circles(img)
 
 
 def showimage(img):
@@ -13,9 +14,36 @@ def showimage(img):
     cv2.destroyAllWindows()
 
 
-def greyscaleimage(img):
+def grayscaleimage(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return gray
 
 
-readimage()
+def circles(img):
+    output = img.copy
+    gray = grayscaleimage(img)
+
+    # detect circles in the image
+    circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100)
+
+    # ensure at least some circles were found
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+
+        # loop over the (x, y) coordinates and radius of the circles
+        for (x, y, r) in circles:
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circle
+            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+            cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+        # show the output image
+        cv2.imshow("output", np.hstack([img, output]))
+        cv2.waitKey(0)
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+readimage(args["image"])
