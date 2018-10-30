a964c82f3d4f2480bfd3549a6192cd72a50dbefc
Manu Mateos <manu.mateos.cruz@gmail.com>
2018-10-30 20:30:44 +0100

    Read image in color, b&w it

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import numpy as np
 import cv2
 
 
-def readimage(file="/Users/manu/photo_2018-06-24_21-03-25.jpg"):
-    img = cv2.imread(file, 0)
+def readimage(path="/Users/manu/photo_2018-06-24_21-03-25.jpg"):
+    img = cv2.imread(path, cv2.IMREAD_COLOR)
+    showimage(greyscaleimage(img))
 
-    cv2.imshow(file, img)
+
+def showimage(img):
+    cv2.imshow("Imagen", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    return
+
+def greyscaleimage(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return gray
 
 
 readimage()
