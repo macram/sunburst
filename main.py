d98a9c8a2b301e694071dc6e27f28e387e72db03
Manu Mateos <manu.mateos.cruz@gmail.com>
2018-12-26 18:44:28 +0100

    Add empty line

--- main.py
+++ main.py
@@ -106,6 +106,7 @@ def processPath(path):
         for file_name in file_list:
             processPath(path + "/" + file_name)
 
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
 args = vars(ap.parse_args())
